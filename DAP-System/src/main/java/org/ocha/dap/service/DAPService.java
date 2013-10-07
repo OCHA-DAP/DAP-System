417e514e939a870b3b52f153a1c1152113f66669
seustachi <eustachi@gmail.com>
2013-10-07 19:35:34 +0200

    adding some user management, not complete yet

--- DAP-System/src/main/java/org/ocha/dap/service/DAPService.java
+++ DAP-System/src/main/java/org/ocha/dap/service/DAPService.java
@@ -1,6 +1,11 @@
 package org.ocha.dap.service;
 
+import org.ocha.dap.security.exception.AuthenticationException;
+
 public interface DAPService {
+	
 	public void getFilesFromCKAN();
+	
+	public void authenticate(final String id, final String password) throws AuthenticationException;
 
 }
--- DAP-System/src/main/java/org/ocha/dap/service/DAPServiceImpl.java
+++ DAP-System/src/main/java/org/ocha/dap/service/DAPServiceImpl.java
@@ -1,11 +1,23 @@
 package org.ocha.dap.service;
 
+import org.ocha.dap.persistence.dao.UserDAO;
+import org.ocha.dap.security.exception.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DAPServiceImpl implements DAPService {
+
+	@Autowired
+	private UserDAO userDao;
 
 	@Override
 	public void getFilesFromCKAN() {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	@Override
+	public void authenticate(final String id, final String password) throws AuthenticationException {
+		userDao.authenticate(id, password);
 	}
 
 }
