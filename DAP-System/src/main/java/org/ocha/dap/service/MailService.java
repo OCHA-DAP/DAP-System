2161cdaa171e0703b8ea24b08acdbf390d6a0468
seustachi <eustachi@gmail.com>
2013-10-24 13:42:22 +0200

    Sending mails on validation failure

---
+++ DAP-System/src/main/java/org/ocha/dap/service/MailService.java
@@ -0,0 +1,14 @@
+package org.ocha.dap.service;
+
+import org.ocha.dap.persistence.entity.CKANDataset;
+
+
+public interface MailService {
+	
+	public void sendMail(final String recipient, final String mailSubject, final String mailBody);
+	
+	public void sendMailForResourceEvaluationFailure(final String id, final String revision_id, final CKANDataset.Type evaluator);
+	
+	public String getMaintainerForFile(final String id, final String revision_id);
+
+}
