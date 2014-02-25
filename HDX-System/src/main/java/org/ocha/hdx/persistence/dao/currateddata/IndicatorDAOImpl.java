52785924929cadea8e8feef3c948b173fe07d790
Dan Mihaila <danmihaila@gmail.com>
2014-02-25 13:13:58 +0200

    #24 Write a more complete test for storage of Indicator.value types

--- HDX-System/src/main/java/org/ocha/hdx/persistence/dao/currateddata/IndicatorDAOImpl.java
+++ HDX-System/src/main/java/org/ocha/hdx/persistence/dao/currateddata/IndicatorDAOImpl.java
@@ -24,7 +24,7 @@ public class IndicatorDAOImpl implements IndicatorDAO {
 
 	@Override
 	public List<Indicator> listLastIndicators(final int limit) {
-		final TypedQuery<Indicator> query = em.createQuery("SELECT i FROM Indicator i ORDER BY i.id DESC", Indicator.class).setMaxResults(limit);
+		final TypedQuery<Indicator> query = this.em.createQuery("SELECT i FROM Indicator i ORDER BY i.id DESC", Indicator.class).setMaxResults(limit);
 		return query.getResultList();
 	}
 
@@ -44,7 +44,7 @@ public class IndicatorDAOImpl implements IndicatorDAO {
 		indicator.setInitialValue(initialValue);
 		indicator.setSourceLink(sourceLink);
 		indicator.setImportFromCKAN(importFromCKAN);
-		em.persist(indicator);
+		this.em.persist(indicator);
 	}
 
 	@Override
@@ -55,7 +55,7 @@ public class IndicatorDAOImpl implements IndicatorDAO {
 			sb.append(" AND i.entity.type.code = 'country' AND i.entity.code IN :countryCodes ");
 		}
 		sb.append(" ORDER BY i.start, i.entity.type.code, i.entity.code");
-		final TypedQuery<Indicator> query = em.createQuery(sb.toString(), Indicator.class).setParameter("periodicity", periodicity).setParameter("source", sourceCode)
+		final TypedQuery<Indicator> query = this.em.createQuery(sb.toString(), Indicator.class).setParameter("periodicity", periodicity).setParameter("source", sourceCode)
 				.setParameter("indicatorType", indicatorTypeCode);
 
 		if ((countryCodes != null) && !countryCodes.isEmpty()) {
@@ -67,7 +67,7 @@ public class IndicatorDAOImpl implements IndicatorDAO {
 
 	@Override
 	public List<Indicator> listIndicatorsByPeriodicityAndEntityAndIndicatorType(final Periodicity periodicity, final String entityType, final String entityCode, final String indicatorTypeCode) {
-		final TypedQuery<Indicator> query = em
+		final TypedQuery<Indicator> query = this.em
 				.createQuery(
 						"SELECT i FROM Indicator i WHERE i.periodicity = :periodicity AND i.entity.type.code = :entityType AND i.entity.code = :entityCode AND i.type.code = :indicatorType ORDER BY i.start, i.source.code",
 						Indicator.class).setParameter("periodicity", periodicity).setParameter("entityType", entityType).setParameter("entityCode", entityCode)
@@ -85,7 +85,7 @@ public class IndicatorDAOImpl implements IndicatorDAO {
 			sb.append(" AND i.entity.type.code = 'country' AND i.entity.code IN :countryCodes ");
 		}
 		sb.append(" ORDER BY i.entity.type.code, i.entity.code ");
-		final TypedQuery<Indicator> query = em.createQuery(sb.toString(), Indicator.class).setParameter("periodicity", Periodicity.YEAR).setParameter("start", timeRange.getStart())
+		final TypedQuery<Indicator> query = this.em.createQuery(sb.toString(), Indicator.class).setParameter("periodicity", Periodicity.YEAR).setParameter("start", timeRange.getStart())
 				.setParameter("source", sourceCode).setParameter("indicatorType", indicatorTypeCode);
 
 		if ((countryCodes != null) && !countryCodes.isEmpty()) {
@@ -97,7 +97,7 @@ public class IndicatorDAOImpl implements IndicatorDAO {
 	@Override
 	public List<Indicator> listIndicatorsByYearAndSourceAndIndicatorTypes(final int year, final String sourceCode, final List<String> indicatorTypeCodes) {
 		final TimeRange timeRange = new TimeRange(year);
-		final TypedQuery<Indicator> query = em
+		final TypedQuery<Indicator> query = this.em
 				.createQuery(
 						"SELECT i FROM Indicator i WHERE i.periodicity = :periodicity AND i.source.code = :source AND i.type.code IN :indicatorTypes AND i.start = :start ORDER BY i.entity.type.code, i.entity.code, i.type.code",
 						Indicator.class).setParameter("periodicity", Periodicity.YEAR).setParameter("start", timeRange.getStart()).setParameter("source", sourceCode)
@@ -109,27 +109,38 @@ public class IndicatorDAOImpl implements IndicatorDAO {
 	@Override
 	@Transactional
 	public void deleteAllIndicators() {
-		em.createQuery("DELETE FROM Indicator").executeUpdate();
+		this.em.createQuery("DELETE FROM Indicator").executeUpdate();
 	}
 
 	@Override
 	@Transactional
 	public void deleteAllIndicatorsFromImport(final long importId) {
-		em.createQuery("DELETE FROM Indicator i WHERE i.importFromCKAN.id = :importId").setParameter("importId", importId).executeUpdate();
+		this.em.createQuery("DELETE FROM Indicator i WHERE i.importFromCKAN.id = :importId").setParameter("importId", importId).executeUpdate();
 
 	}
 
 	@Override
 	@Transactional
 	public void deleteIndicator(final long indicatorId) {
-		em.createQuery("DELETE FROM Indicator i WHERE i.id = :indicatorId").setParameter("indicatorId", indicatorId).executeUpdate();
+		this.em.createQuery("DELETE FROM Indicator i WHERE i.id = :indicatorId").setParameter("indicatorId", indicatorId).executeUpdate();
 
+	}
+
+	/**
+	 * @author Dan TODO: It could delete all indicators in one transaction
+	 */
+	@Override
+	@Transactional
+	public void deleteIndicators(final List<Long> indList) {
+		for (final Long ind : indList) {
+			deleteIndicator(ind);
+		}
 	}
 
 	@Override
 	public List<String> getExistingSourcesCodesForYearAndIndicatorType(final int year, final String indicatorTypeCode) {
 		final TimeRange timerange = new TimeRange(Integer.valueOf(year).toString());
-		final TypedQuery<String> query = em
+		final TypedQuery<String> query = this.em
 				.createQuery("SELECT DISTINCT(i.source.code) FROM Indicator i Where i.start = :start AND i.end = :end AND i.periodicity = :periodicity AND i.type.code = :indicatorTypeCode",
 						String.class).setParameter("start", timerange.getStart()).setParameter("end", timerange.getEnd()).setParameter("periodicity", timerange.getPeriodicity())
 				.setParameter("indicatorTypeCode", indicatorTypeCode);
@@ -138,7 +149,7 @@ public class IndicatorDAOImpl implements IndicatorDAO {
 
 	@Override
 	public List<String> getExistingSourcesCodesForIndicatorType(final String indicatorTypeCode) {
-		final TypedQuery<String> query = em.createQuery("SELECT DISTINCT(i.source.code) FROM Indicator i Where i.type.code = :indicatorTypeCode", String.class).setParameter("indicatorTypeCode",
+		final TypedQuery<String> query = this.em.createQuery("SELECT DISTINCT(i.source.code) FROM Indicator i Where i.type.code = :indicatorTypeCode", String.class).setParameter("indicatorTypeCode",
 				indicatorTypeCode);
 		return query.getResultList();
 	}
