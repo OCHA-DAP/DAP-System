5919e4ba791f46f6e525c1f44b857cb8004fd70b
seustachi <eustachi@gmail.com>
2014-03-04 18:33:01 +0100

    just organizing imports

--- HDX-System/src/test/java/org/ocha/hdx/persistence/dao/currateddata/UnitDAOImplTest.java
+++ HDX-System/src/test/java/org/ocha/hdx/persistence/dao/currateddata/UnitDAOImplTest.java
@@ -1,5 +1,7 @@
 package org.ocha.hdx.persistence.dao.currateddata;
 
+import javax.persistence.NoResultException;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,9 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.persistence.NoResultException;
-import java.util.List;
-
 /**
  * @author Alexandru Artimon
  * @since 20/02/14
@@ -22,103 +21,103 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/ctx-config-test.xml", "classpath:/ctx-core.xml", "classpath:/ctx-dao.xml", "classpath:/ctx-persistence-test.xml" })
 public class UnitDAOImplTest {
-    private static Logger logger = LoggerFactory.getLogger(SourceDAOImplTest.class);
-    private final String unit1Name = "unit1";
-    private final String unit1Code = "unit1Code";
+	private static Logger logger = LoggerFactory.getLogger(SourceDAOImplTest.class);
+	private final String unit1Name = "unit1";
+	private final String unit1Code = "unit1Code";
 
-    private final String unit2Name = "unit2";
-    private final String unit2Code = "unit2Code";
+	private final String unit2Name = "unit2";
+	private final String unit2Code = "unit2Code";
 
-    @Autowired
-    private UnitDAO unitDAO;
+	@Autowired
+	private UnitDAO unitDAO;
 
-    @Autowired
-    private TextDAO textDAO;
+	@Autowired
+	private TextDAO textDAO;
 
-    @Test
-    public void testCreateDeleteUnit(){
-        logger.info("Testing create and delete for Unit ...");
+	@Test
+	public void testCreateDeleteUnit() {
+		logger.info("Testing create and delete for Unit ...");
 
-        final Text unit1Text = textDAO.createText(unit1Name);
-        final Unit unit1 = unitDAO.createUnit(unit1Code, unit1Text);
-        final Unit unit1Copy = unitDAO.getUnitByCode(unit1Code);
+		final Text unit1Text = textDAO.createText(unit1Name);
+		final Unit unit1 = unitDAO.createUnit(unit1Code, unit1Text);
+		final Unit unit1Copy = unitDAO.getUnitByCode(unit1Code);
 
-        Assert.assertEquals(unit1.getId(), unit1Copy.getId());
-        Assert.assertEquals(unit1Name, unit1Copy.getName().getDefaultValue());
-        Assert.assertEquals(1, unitDAO.listUnits().size());
+		Assert.assertEquals(unit1.getId(), unit1Copy.getId());
+		Assert.assertEquals(unit1Name, unit1Copy.getName().getDefaultValue());
+		Assert.assertEquals(1, unitDAO.listUnits().size());
 
-        unitDAO.updateUnit(unit1.getId(), unit2Name);
+		unitDAO.updateUnit(unit1.getId(), unit2Name);
 
-        final Unit unitUpdated = unitDAO.getUnitById(unit1.getId());
-        Assert.assertEquals(unit1.getId(), unitUpdated.getId());
-        Assert.assertEquals(unit1.getCode(), unitUpdated.getCode());
-        Assert.assertEquals(unit2Name, unitUpdated.getName().getDefaultValue());
-        Assert.assertFalse(unit1.getName().getDefaultValue().equals(unitUpdated.getName().getDefaultValue()));
+		final Unit unitUpdated = unitDAO.getUnitById(unit1.getId());
+		Assert.assertEquals(unit1.getId(), unitUpdated.getId());
+		Assert.assertEquals(unit1.getCode(), unitUpdated.getCode());
+		Assert.assertEquals(unit2Name, unitUpdated.getName().getDefaultValue());
+		Assert.assertFalse(unit1.getName().getDefaultValue().equals(unitUpdated.getName().getDefaultValue()));
 
-        //cleanup
+		// cleanup
 
-        unitDAO.deleteUnit(unit1Copy.getId());
-        try{
-            unitDAO.getUnitByCode(unit1Code);
-            Assert.fail("Unit shouldn't exist");
-        } catch(NoResultException e) {
-            //ok
-        }
-        textDAO.deleteText(unit1Text.getId());
+		unitDAO.deleteUnit(unit1Copy.getId());
+		try {
+			unitDAO.getUnitByCode(unit1Code);
+			Assert.fail("Unit shouldn't exist");
+		} catch (final NoResultException e) {
+			// ok
+		}
+		textDAO.deleteText(unit1Text.getId());
 
-    }
+	}
 
-    @Test
-    public void testListFindUnit(){
-        logger.info("Testing list and find for Unit ...");
-        Assert.assertEquals(0, unitDAO.listUnits().size());
+	@Test
+	public void testListFindUnit() {
+		logger.info("Testing list and find for Unit ...");
+		Assert.assertEquals(0, unitDAO.listUnits().size());
 
-        final Text unit1Text = textDAO.createText(unit1Name);
-        final Unit unit1 = unitDAO.createUnit(unit1Code, unit1Text);
+		final Text unit1Text = textDAO.createText(unit1Name);
+		final Unit unit1 = unitDAO.createUnit(unit1Code, unit1Text);
 
-        Assert.assertEquals(1, unitDAO.listUnits().size());
+		Assert.assertEquals(1, unitDAO.listUnits().size());
 
-        final Text unit2Text = textDAO.createText(unit2Name);
-        final Unit unit2 = unitDAO.createUnit(unit2Code, unit2Text);
+		final Text unit2Text = textDAO.createText(unit2Name);
+		final Unit unit2 = unitDAO.createUnit(unit2Code, unit2Text);
 
-        Assert.assertEquals(2, unitDAO.listUnits().size());
+		Assert.assertEquals(2, unitDAO.listUnits().size());
 
-        //test get unit by code method
-        final Unit unit1Copy = unitDAO.getUnitByCode(unit1Code);
+		// test get unit by code method
+		final Unit unit1Copy = unitDAO.getUnitByCode(unit1Code);
 
-        Assert.assertEquals(unit1.getId(), unit1Copy.getId());
-        Assert.assertEquals(unit1.getCode(), unit1Copy.getCode());
-        Assert.assertEquals(unit1.getName().getDefaultValue(), unit1Copy.getName().getDefaultValue());
+		Assert.assertEquals(unit1.getId(), unit1Copy.getId());
+		Assert.assertEquals(unit1.getCode(), unit1Copy.getCode());
+		Assert.assertEquals(unit1.getName().getDefaultValue(), unit1Copy.getName().getDefaultValue());
 
-        //test get unit by id method
-        final Unit unit2Copy = unitDAO.getUnitById(unit2.getId());
+		// test get unit by id method
+		final Unit unit2Copy = unitDAO.getUnitById(unit2.getId());
 
-        Assert.assertEquals(unit2.getId(), unit2Copy.getId());
-        Assert.assertEquals(unit2.getCode(), unit2Copy.getCode());
-        Assert.assertEquals(unit2.getName().getDefaultValue(), unit2Copy.getName().getDefaultValue());
+		Assert.assertEquals(unit2.getId(), unit2Copy.getId());
+		Assert.assertEquals(unit2.getCode(), unit2Copy.getCode());
+		Assert.assertEquals(unit2.getName().getDefaultValue(), unit2Copy.getName().getDefaultValue());
 
-        unitDAO.deleteUnit(unit1.getId());
-        try{
-            unitDAO.getUnitByCode(unit1Code);
-            Assert.fail("Unit shouldn't exist");
-        } catch(NoResultException e) {
-            //ok
-        }
-        Assert.assertEquals(1, unitDAO.listUnits().size());
+		unitDAO.deleteUnit(unit1.getId());
+		try {
+			unitDAO.getUnitByCode(unit1Code);
+			Assert.fail("Unit shouldn't exist");
+		} catch (final NoResultException e) {
+			// ok
+		}
+		Assert.assertEquals(1, unitDAO.listUnits().size());
 
-        unitDAO.deleteUnit(unit2.getId());
-        try{
-            unitDAO.getUnitByCode(unit2Code);
-            Assert.fail("Unit shouldn't exist");
-        } catch(NoResultException e) {
-            //ok
-        }
-        Assert.assertEquals(0, unitDAO.listUnits().size());
+		unitDAO.deleteUnit(unit2.getId());
+		try {
+			unitDAO.getUnitByCode(unit2Code);
+			Assert.fail("Unit shouldn't exist");
+		} catch (final NoResultException e) {
+			// ok
+		}
+		Assert.assertEquals(0, unitDAO.listUnits().size());
 
-        //cleanup
-        textDAO.deleteText(unit1Text.getId());
-        textDAO.deleteText(unit2Text.getId());
+		// cleanup
+		textDAO.deleteText(unit1Text.getId());
+		textDAO.deleteText(unit2Text.getId());
 
-    }
+	}
 
 }
