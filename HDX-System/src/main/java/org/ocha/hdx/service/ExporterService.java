e962f3fb11d4ed37a33c2e89783c0e1516c6051a
bmichiels <benoit.michiels@gmail.com>
2014-03-19 08:18:04 +0100

    Classes and DB views for indicator-centric report generation (+ cosmetic)

--- HDX-System/src/main/java/org/ocha/hdx/service/ExporterService.java
+++ HDX-System/src/main/java/org/ocha/hdx/service/ExporterService.java
@@ -6,7 +6,10 @@ import java.util.Map;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.ocha.hdx.exporter.country.ExporterCountryQueryData;
 import org.ocha.hdx.exporter.helper.ReportRow;
+import org.ocha.hdx.exporter.indicator.ExporterIndicatorQueryData;
 import org.ocha.hdx.persistence.entity.curateddata.IndicatorType;
+import org.ocha.hdx.persistence.entity.view.IndicatorData;
+import org.ocha.hdx.persistence.entity.view.IndicatorTypeOverview;
 
 /**
  * Exporter service.
@@ -24,27 +27,50 @@ public interface ExporterService {
 
 	public IndicatorType getIndicatorTypeByCode(final String code);
 
-	/*
-	 * Country reports.
-	 */
+	/* **************** */
+	/* Country reports. */
+	/* **************** */
 	public XSSFWorkbook exportCountry_XLSX(String countryCode, Integer fromYear, Integer toYear, String language);
 
-	// Country overview
+	/*
+	 *  Country overview
+	 */
 	public List<Object[]> getCountryOverviewData(final ExporterCountryQueryData queryData);
 
 	/**
-	 * 
 	 * Only the rows with actual data are added in the returned result.
 	 * 
 	 * @param queryData
 	 * @return a map of the rows we can expect in the report.
 	 */
 	public Map<String, ReportRow> getCountryCrisisHistoryData(ExporterCountryQueryData queryData);
+
 	public Map<String, ReportRow> getCountrySocioEconomicData(ExporterCountryQueryData queryData);
+
 	public Map<String, ReportRow> getCountryVulnerabilityData(ExporterCountryQueryData queryData);
+
 	public Map<String, ReportRow> getCountryCapacityData(ExporterCountryQueryData queryData);
+
 	public Map<String, ReportRow> getCountryOtherData(ExporterCountryQueryData queryData);
 
 	public Map<String, ReportRow> getCountry5YearsData(ExporterCountryQueryData queryData);
+
+	/* ****************** */
+	/* Indicator reports. */
+	/* ****************** */
+	public XSSFWorkbook exportIndicator_XLSX(String indicatorTypeCode, String sourceCode, Long fromYear, Long toYear, String language);
+
+	/*
+	 *  Indicator overview
+	 */
+	public IndicatorTypeOverview getIndicatorTypeOverviewData(ExporterIndicatorQueryData queryData);
+
+	/**
+	 * Only the rows with actual data are added in the returned result.
+	 * 
+	 * @param queryData
+	 * @return a map of the rows we can expect in the report.
+	 */
+	public Map<Long, Map<String, IndicatorData>> getIndicatorDataData(ExporterIndicatorQueryData queryData);
 
 }
