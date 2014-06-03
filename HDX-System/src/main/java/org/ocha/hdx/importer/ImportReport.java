f630d6743c011c5445431c8264f16a77877079cd
seustachi <eustachi@gmail.com>
2014-06-03 09:32:38 +0200

    adding an import report

---
+++ HDX-System/src/main/java/org/ocha/hdx/importer/ImportReport.java
@@ -0,0 +1,15 @@
+package org.ocha.hdx.importer;
+
+public class ImportReport {
+
+	private boolean overallResult;
+
+	public boolean getOverallResult() {
+		return overallResult;
+	}
+
+	public void setOverallResult(final boolean overallResult) {
+		this.overallResult = overallResult;
+	}
+
+}
