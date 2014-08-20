c5c1b7e837c17ac72ea9110ecaafacd7b9b4b2f5
seustachi <eustachi@gmail.com>
2014-08-20 16:22:10 +0200

    https://github.com/OCHA-DAP/DAP-System/issues/266

--- HDX-System/src/main/java/org/ocha/hdx/rest/ApiV2Resource.java
+++ HDX-System/src/main/java/org/ocha/hdx/rest/ApiV2Resource.java
@@ -10,6 +10,9 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.ocha.hdx.model.api2.ApiIndicatorValue;
+import org.ocha.hdx.model.api2.ApiResultWrapper;
+import org.ocha.hdx.service.ApiV2BackendService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @RolesAllowed({ "admin", "api" })
@@ -17,12 +20,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class ApiV2Resource {
 
+	@Autowired
+	private ApiV2BackendService apiV2BackendService;
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/values")
-	public List<ApiIndicatorValue> getIndicatorValues(@QueryParam("e") final List<String> entityCodes, @QueryParam("it") final List<String> indicatorTypeCodes, @QueryParam("s") final List<String> sourceCodes,
-			@QueryParam("minTime") final String minTime, @QueryParam("maxTime") final String maxTime) {
-		return null;
+	public ApiResultWrapper<ApiIndicatorValue> getIndicatorValues(@QueryParam("e") final List<String> entityCodes, @QueryParam("it") final List<String> indicatorTypeCodes,
+			@QueryParam("s") final List<String> sourceCodes, @QueryParam("minTime") final String minTime, @QueryParam("maxTime") final String maxTime, @QueryParam("pageNum") final Integer pageNum,
+			@QueryParam("pageSize") final Integer pageSize, @QueryParam("lang") final String lang) {
+
+		// We could let Jersey do this for us, but we might want to do something smarter in the future
+		final Integer startYear = minTime == null ? null : new Integer(minTime);
+		final Integer endYear = maxTime == null ? null : new Integer(maxTime);
+
+		final ApiResultWrapper<ApiIndicatorValue> listIndicatorsByCriteriaWithPagination = apiV2BackendService.listIndicatorsByCriteriaWithPagination(indicatorTypeCodes, sourceCodes, entityCodes,
+				startYear, endYear, pageNum, pageSize, lang);
+
+		return listIndicatorsByCriteriaWithPagination;
 	}
 
 }
