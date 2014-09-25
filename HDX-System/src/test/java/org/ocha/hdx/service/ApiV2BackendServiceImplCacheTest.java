70fa2c8e6bab90149f50e7ef50ed82764a84db57
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2014-09-25 18:24:36 +0300

    #288 adding junit test
    - unfortunately derby doesn't support sorting on blob/clob types

--- HDX-System/src/test/java/org/ocha/hdx/service/ApiV2BackendServiceImplCacheTest.java
+++ HDX-System/src/test/java/org/ocha/hdx/service/ApiV2BackendServiceImplCacheTest.java
@@ -5,8 +5,10 @@ package org.ocha.hdx.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Date;
 
 import javax.annotation.Resource;
@@ -20,6 +22,7 @@ import org.ocha.hdx.model.api2.ApiIndicatorValue;
 import org.ocha.hdx.model.api2.ApiResultWrapper;
 import org.ocha.hdx.model.api2util.RequestParamsWrapper;
 import org.ocha.hdx.model.api2util.RequestParamsWrapper.PeriodType;
+import org.ocha.hdx.model.api2util.RequestParamsWrapper.SortingOption;
 import org.ocha.hdx.model.validation.ValidationStatus;
 import org.ocha.hdx.persistence.dao.ImportFromCKANDAO;
 import org.ocha.hdx.persistence.dao.currateddata.EntityDAO;
@@ -168,6 +171,114 @@ public class ApiV2BackendServiceImplCacheTest {
 
 		this.indicatorDAO.deleteAllIndicatorsFromImport(importFromCKAN.getId());
 		this.importFromCKANDAO.deleteImportFromCKAN(importFromCKAN.getId());
+	}
+
+	@Test
+	public final void testListIndicatorsSorted() {
+		final ImportFromCKAN importFromCKAN = this.importFromCKANDAO.createNewImportRecord("anyResourceId", "anyRevisionId", new Date());
+		final Source src1 = this.sourceDAO.getSourceByCode("esa-unpd-WPP2012");
+		final IndicatorType type1 = this.indicatorTypeDAO.getIndicatorTypeByCode("PSP080");
+		final Entity entity1 = this.entityDAO.getEntityByCodeAndType("Fi", "country");
+
+		final LocalDateTime dateTime2012 = new LocalDateTime(2012, 1, 1, 0, 0);
+		final Date date2012 = dateTime2012.toDate();
+		final Date date2013 = dateTime2012.plusYears(1).toDate();
+
+		this.indicatorDAO.createIndicator(src1, entity1, type1, date2012, date2012, Periodicity.MONTH, new IndicatorValue(10000.0), "10000$", ValidationStatus.SUCCESS, "http://www.example.com",
+				importFromCKAN);
+
+		final Source src2 = this.sourceDAO.getSourceByCode("acled");
+		final Entity entity2 = this.entityDAO.getEntityByCodeAndType("Ro", "country");
+
+		this.indicatorDAO.createIndicator(src2, entity2, type1, date2013, date2013, Periodicity.MONTH, new IndicatorValue(20000.0), "20000$", ValidationStatus.SUCCESS, "http://www.example2.com",
+				importFromCKAN);
+
+		this.indicatorDAO.createIndicator(src2, entity2, type1, date2012, date2013, Periodicity.MONTH, new IndicatorValue(30000.0), "30000$", ValidationStatus.SUCCESS, "http://www.example3.com",
+				importFromCKAN);
+
+
+		this.checkSorting(SortingOption.VALUE_ASC, new Comparator<ApiIndicatorValue>() {
+
+			@Override
+			public int compare(final ApiIndicatorValue o1, final ApiIndicatorValue o2) {
+				return new Double(o1.getValue()).compareTo(new Double(o2.getValue()));
+			}
+		});
+
+		this.checkSorting(SortingOption.VALUE_DESC, new Comparator<ApiIndicatorValue>() {
+
+			@Override
+			public int compare(final ApiIndicatorValue o1, final ApiIndicatorValue o2) {
+				return new Double(o2.getValue()).compareTo(new Double(o1.getValue()));
+			}
+		});
+
+//		this.checkSorting(SortingOption.COUNTRY_ASC, new Comparator<ApiIndicatorValue>() {
+//
+//			@Override
+//			public int compare(final ApiIndicatorValue o1, final ApiIndicatorValue o2) {
+//				return o1.getLocationName().compareTo(o2.getLocationName());
+//			}
+//		});
+//
+//		this.checkSorting(SortingOption.COUNTRY_DESC, new Comparator<ApiIndicatorValue>() {
+//
+//			@Override
+//			public int compare(final ApiIndicatorValue o1, final ApiIndicatorValue o2) {
+//				return o2.getLocationName().compareTo(o1.getLocationName());
+//			}
+//		});
+
+//		this.checkSorting(SortingOption.SOURCE_TYPE_ASC, new Comparator<ApiIndicatorValue>() {
+//
+//			@Override
+//			public int compare(final ApiIndicatorValue o1, final ApiIndicatorValue o2) {
+//				return o1.getSourceName().compareTo(o2.getSourceName());
+//			}
+//		});
+//
+//		this.checkSorting(SortingOption.SOURCE_TYPE_DESC, new Comparator<ApiIndicatorValue>() {
+//
+//			@Override
+//			public int compare(final ApiIndicatorValue o1, final ApiIndicatorValue o2) {
+//				return o2.getSourceName().compareTo(o1.getSourceName());
+//			}
+//		});
+//
+//		this.checkSorting(SortingOption.INDICATOR_TYPE_ASC, new Comparator<ApiIndicatorValue>() {
+//
+//			@Override
+//			public int compare(final ApiIndicatorValue o1, final ApiIndicatorValue o2) {
+//				return o1.getIndicatorTypeName().compareTo(o2.getIndicatorTypeName());
+//			}
+//		});
+//
+//		this.checkSorting(SortingOption.INDICATOR_TYPE_DESC, new Comparator<ApiIndicatorValue>() {
+//
+//			@Override
+//			public int compare(final ApiIndicatorValue o1, final ApiIndicatorValue o2) {
+//				return o2.getIndicatorTypeName().compareTo(o1.getIndicatorTypeName());
+//			}
+//		});
+
+
+		this.indicatorDAO.deleteAllIndicatorsFromImport(importFromCKAN.getId());
+		this.importFromCKANDAO.deleteImportFromCKAN(importFromCKAN.getId());
+	}
+
+	private void checkSorting(final SortingOption sortingOption,
+			final Comparator<ApiIndicatorValue> comparator) {
+
+		final ApiResultWrapper<ApiIndicatorValue> resultWrapper =
+				this.apiV2BackendService.listIndicatorsByCriteriaWithPagination(Arrays.asList("PSP080", "FY620"),
+				Arrays.asList("esa-unpd-WPP2012", "acled"), Arrays.asList("Ro", "Fi"), null, null, null, sortingOption, 0, 4, "en");
+
+		assertTrue("There should be at least 2 results", resultWrapper.getResults().size()>2);
+		for (int i = 1; i < resultWrapper.getResults().size(); i++) {
+			final ApiIndicatorValue indicatorValue1 = resultWrapper.getResults().get(i-1);
+			final ApiIndicatorValue indicatorValue2 = resultWrapper.getResults().get(i);
+			assertTrue(comparator.compare(indicatorValue1, indicatorValue2) <= 0 );
+		}
 	}
 
 }
